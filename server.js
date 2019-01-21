f4678a2a33463d53a138c215ab3c054e828338dc
kambala yashwanth <kambaalayashwanth@gmail.com>
2019-01-21 11:02:23 +0530

    remoed test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use('/restaurant', restaurantRoutes)
 
-app.get('/a',(req,res)=>res.send("s"))
+
 
 const server = app.listen(4000, () => console.info(`Example app listening on port 4000!`))
  
